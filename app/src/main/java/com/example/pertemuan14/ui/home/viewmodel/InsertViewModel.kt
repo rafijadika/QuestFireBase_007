b0092ea2deba6487a9c2911b5c72838f010e6961
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-11 23:31:33 +0700

    membuat class InsertViewModel dan membuat fun validatefields

--- app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.pertemuan14.repository.RepositoryMhs
 import kotlinx.coroutines.launch
 
 class InsertViewModel (
@@ -22,6 +21,24 @@ class InsertViewModel (
             insertUiEvent = mahasiswaEvent,
         )
     }
+
+    // Validasi data input pengguna
+    fun validateFields() : Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim?.isNotEmpty() == true) null else "NIM tidak boleh kosong",
+            nama = if (event.nama?.isNotEmpty() == true) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin?.isNotEmpty() == true) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat?.isNotEmpty() == true) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas?.isNotEmpty() == true) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan?.isNotEmpty() == true) null else "angkatan tidak boleh kosong"
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+
+
 
 
 )
