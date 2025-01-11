9565326fa1aa401fc13aacf167d4b9a1c83df534
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-12 04:52:34 +0700

    memperbaiki PengelolaHalaman

--- app/src/main/java/com/example/pertemuan14/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/PenyediaViewModel.kt
@@ -4,18 +4,24 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.pertemuan14.MahasiswaApplications
+import com.example.pertemuan14.MahasiswaApp
 import com.example.pertemuan14.ui.home.viewmodel.HomeViewModel
+import com.example.pertemuan14.ui.home.viewmodel.InsertViewModel
+
+//import com.umy.pam_firebase.di.Mhs
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(
-                MahasiswaApplications().container.repositoryMhs
-            )
+            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp
+            HomeViewModel(app.containerApp.repositoryMhs)
+        }
+        initializer {
+            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp
+            InsertViewModel(app.containerApp.repositoryMhs)
         }
     }
 }
 
-fun CreationExtras.MahasiswaApplications(): MahasiswaApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+//fun CreationExtras.mahasiswaApp(): MahasiswaApp =
+//    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApp)
