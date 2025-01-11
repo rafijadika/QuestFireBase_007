55fd44645fdda18f8ad9e32251f147c0eeff9d9d
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-11 23:29:50 +0700

    membuat file InsertViewModel

---
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.pertemuan14.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs // deklarasi variabel mhs
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state by input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent =uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+
+)
