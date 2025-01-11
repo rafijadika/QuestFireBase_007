99d1c3273d485a90ef8667f7b021d864faca46be
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-11 23:40:17 +0700

    mengisi fungsi NetworkRepositryMhs.kt

--- app/src/main/java/com/example/pertemuan14/repository/NetworkRepositryMhs.kt
+++ app/src/main/java/com/example/pertemuan14/repository/NetworkRepositryMhs.kt
@@ -6,41 +6,66 @@ import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
-class NetworkRepositoryMhs (
+class NetworkRepositoryMhs(
     private val firestore: FirebaseFirestore
-):RepositoryMhs {
+) : RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa)
+                .await() //Membuat Method untuk  Insert Data kedalam Collection Mahasiswa
+        } catch (e: Exception) {
+            throw Exception("Gagal menambahkan data mahasiswa : ${e.message}") //Tambahkan Debugging untuk excpetion error
+        }
     }
 
     override fun getAllMhs(): Flow<List<Mahasiswa>> = callbackFlow {
+        // Membuka collection dari Firestore
         val mhsCollection = firestore.collection("Mahasiswa")
             .orderBy("nim", Query.Direction.ASCENDING)
-            .addSnapshotListener() { value, error ->
+            .addSnapshotListener { value, error ->
                 if (value != null) {
                     val mhsList = value.documents.mapNotNull {
-                        it.toObject(Mahasiswa::class.java)
+                        // Convert dari Document Firestore kedalam data class
+                        it.toObject(Mahasiswa::class.java)!!
                     }
+                    // fungsi untuk mengirimkan collection ke database
                     trySend(mhsList)
                 }
-
             }
-
         awaitClose {
+            // Menutup Collection
             mhsCollection.remove()
         }
     }
 
-    override fun getMhs(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override fun getMhs(nim: String): Flow<Mahasiswa> = callbackFlow{
+        val mhsDocument = firestore.collection("Mahasiswa") // Method untuk get Mahasiswa by nim
+            .document(nim)
+            .addSnapshotListener{ value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocument.remove()
+        }
     }
-}
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        try {
+            mahasiswa.nim?.let {
+                firestore.collection("Mahasiswa") // Method untuk hapus mahasiswa
+                    .document(it)
+                    .delete()
+                    .await()
+            }
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa: ${e.message}")
+        }
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        try {
