695612a3095de481b90e1ef8a0a972243f7c83b3
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-12 04:57:23 +0700

    membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/pertemuan14/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,42 @@
+package com.example.pertemuan14.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan14.ui.home.pages.HomeView
+import com.example.pertemuan14.ui.home.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                onDetailClick = { nim ->
+                    navController.navigate(DestinasiDetail.createRoute(nim)) // navigate to detail view
+                }
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+        
+    }
+}
