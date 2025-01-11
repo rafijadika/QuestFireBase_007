b39d5817fecb2e7e29abe2dae6a8e150433f691a
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-11 23:34:58 +0700

    membuat data class mahasiswa event

--- app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -40,5 +40,13 @@ class InsertViewModel (
 
 
 
-
+//data class variabel yang menyimpan data input form
+data class MahasiswaEvent(
+    val nim: String? = "",
+    val nama: String? = "",
+    val jenisKelamin: String? = "",
+    val alamat: String? = "",
+    val kelas: String? = "",
+    val angkatan: String? = ""
 )
+
