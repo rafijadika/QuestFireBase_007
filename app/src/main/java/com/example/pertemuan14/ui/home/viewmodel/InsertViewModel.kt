732803db3f06e911ef196931b79da0705bc5dda6
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2025-01-12 04:51:28 +0700

    membuat InsertViewModel

--- app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/home/viewmodel/InsertViewModel.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.model.Mahasiswa
+import com.example.pertemuan14.repository.RepositoryMhs
 import kotlinx.coroutines.launch
 
 class InsertViewModel (
@@ -36,9 +38,62 @@ class InsertViewModel (
         uiEvent = uiEvent.copy(isEntryValid = errorState)
         return errorState.isValid()
     }
+    // Fungsi insert view model
+    fun insertMhs() {
+        if (validateFields()) { // logika validasi insert
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMhs(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception) {
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
 
+    // fungsi reset form
+    fun resetForm() {
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
 
+    // fungsi reset snack bar message
+    fun resetSnackBarMessage() {
+        uiState = FormState.Idle
+    }
+}
+// membuat sealed class form state
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
 
+// data class Insert Ui State
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
+// data class Form Error State
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
 
 //data class variabel yang menyimpan data input form
 data class MahasiswaEvent(
@@ -50,3 +105,12 @@ data class MahasiswaEvent(
     val angkatan: String? = ""
 )
 
+// Menyimpan input form kedalam entity
+fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
+    nim = nim?: "",
+    nama = nama?: "",
+    jenisKelamin = jenisKelamin?: "",
+    alamat = alamat?: "",
+    kelas = kelas?: "",
+    angkatan = angkatan?: ""
+)
